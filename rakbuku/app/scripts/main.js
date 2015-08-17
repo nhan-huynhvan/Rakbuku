d2a06250ec95f221de1fb03afb5022be86c775f2
Nhan <nhan.huynhvan@asnet.com.vn>
2015-08-17 17:51:21 +0700

    Update Bourbon practice

--- rakbuku/app/scripts/main.js
+++ rakbuku/app/scripts/main.js
@@ -19,8 +19,8 @@ var Rakbuku = {};
 						menu = $('.menu-items'),
 						heroInitLeft = $('.hero__indct--left'),
 						heroInitRight = $('.hero__indct--right');
-						logo.addClass('js-bounceInDown');
 
+					logo.addClass('js-bounceInDown');
 					setTimeout( function() { 
 						menu.addClass('js-fadeInRight');
 					}, 1000);
@@ -36,33 +36,124 @@ var Rakbuku = {};
 
 				menuActive = function () {
 					$('.menu-items .item__link').on('click', function(event) {
-						var isActive = $(this).attr('href');
-						switch(isActive) {
-							case '#home':
-								itemsActive('.logo');
-								itemsActive('.hero');								
-								break;
-							case '#services':
-								itemsActive('.is-services');
-								break;
-							case '#outwork':
-								itemsActive('.is-outwork');
-								break;
-							case '#about':
-								itemsActive('.is-about');
-								break;
-							case '#contact':
-								itemsActive('.is-contact');
-								break;
-						}
+						if (!$(this).hasClass('item__link-is-active')) {
+
+							var isActive = $(this).attr('href');
+							$('.menu-items .item__link').removeClass('item__link-is-active');
+							$(this).addClass('item__link-is-active');
+
+							switch(isActive) {
+								case '#home':
+									if ($('.logo').hasClass('none-display')) {
+										$('.logo').removeClass('none-display');
+									};
+									itemsActive('.hero');								
+									break;
+								case '#services':
+									if (!$('.logo').hasClass('none-display')) {
+										$('.logo').addClass('none-display');
+									};
+									itemsActive('.is-services');
+									servicesActive();
+									break;
+								case '#outwork':
+									if (!$('.logo').hasClass('none-display')) {
+										$('.logo').addClass('none-display');
+									};
+									itemsActive('.is-outwork');
+									outworkActive();
+									break;
+								case '#about':
+									if (!$('.logo').hasClass('none-display')) {
+										$('.logo').addClass('none-display');
+									};
+									itemsActive('.is-about');
+									aboutActive();
+									break;
+								case '#contact':
+									if (!$('.logo').hasClass('none-display')) {
+										$('.logo').addClass('none-display');
+									};
+									itemsActive('.is-contact');
+									break;
+							}
+
+						};
+
 					});
 				},
 
+				servicesActive = function() {
+
+					var webDesign = $('.js-webdesign'),
+						webDevelopment = $('.js-webdevelopment'),
+						webResponsive = $('.js-responsive'),
+						pcImage = $('.services__colum--images-image--pc'),
+						tabletImage = $('.services__colum--images-image--tablet'),
+						mobileImage = $('.services__colum--images-image--mobile');
+
+						webDesign.removeClass('is-fadeInLeft');
+						webDevelopment.removeClass('is-fadeInLeft');
+						webResponsive.removeClass('is-fadeInLeft');
+						pcImage.removeClass('is-fadeInDownBig');
+						tabletImage.removeClass('is-fadeInDownBig');
+						mobileImage.removeClass('is-fadeInDownBig');
+
+						setTimeout( function() {
+							webDesign.addClass('is-fadeInLeft');
+							pcImage.addClass('is-fadeInDownBig');
+						}, 1000);
+						setTimeout( function() { 
+							webDevelopment.addClass('is-fadeInLeft');
+							tabletImage.addClass('is-fadeInDownBig');
+						}, 2000);
+						setTimeout( function() { 
+							webResponsive.addClass('is-fadeInLeft');
+							mobileImage.addClass('is-fadeInDownBig');
+						}, 3000);
+
+				},
+
+				aboutActive = function () {
+					var ceo = $('.js-ceo'),
+						designer = $('.js-designer'),
+						developer = $('.js-developer'),
+						aboutDisc = $('.about__div--disc'),
+						aboutList = $('.about-title-list');
+
+					ceo.removeClass('is-fadeIn');
+					designer.removeClass('is-fadeIn');
+					developer.removeClass('is-fadeIn');
+					aboutDisc.removeClass('is-bounceInDown');
+					aboutList.removeClass('is-bounceIn');
+
+					setTimeout( function() {
+						aboutDisc.addClass('is-bounceInDown');
+					}, 1000);
+					setTimeout( function() {
+						aboutList.addClass('is-bounceIn');
+					}, 2000);
+					setTimeout( function() {
+						ceo.addClass('is-fadeIn');
+					}, 3000);
+					setTimeout( function() { 
+						designer.addClass('is-fadeIn');
+					}, 4000);
+					setTimeout( function() { 
+						developer.addClass('is-fadeIn');
+					}, 5000);
+				}, 
+
+				outworkActive = function() {
+					var outworkItems = $('.outworks__item');
+					for(var i = 0, len = outworkItems.length; i < len; i ++) {
+						$(outworkItems[i]).addClass('is-bounceIn');
+					}
+				},
+
 				itemsActive = function( item ) {
-					var items = ['.logo', '.hero', '.is-services', '.is-about', '.is-outwork', '.is-contact'];
-					console.log(item);
+					var items = ['.hero', '.is-services', '.is-about', '.is-outwork', '.is-contact'];
 					for(var i = 0, len = items.length; i < len; i ++) {
-						// console.log(items[i]);
 						if (items[i] != item) {
 							if (!$(items[i]).hasClass('none-display')) {
 								$(items[i]).addClass('none-display');
@@ -73,10 +164,7 @@ var Rakbuku = {};
 							} 
 						}
 					}
-					console.log($('.logo'));
 				};
-
-
 
 
 		//###############################
