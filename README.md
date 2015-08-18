0f36080e150d07a176baff57dd8ab252c4ab2c7d
NhanHuynh <nhan.huynhvan@asnet.com.vn>
2015-08-18 20:49:28 +0700

    Update README.md file to user manual

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Rakbuku
+	- An example for Bourbon, Neat, Bitters, Refills
+# Install
+	- Clone sourecode: https://github.com/nhanhv/Rakbuku.git
+	- Run 'vagrant up'
+	- Run 'vagrant ssh'
+	- Run 'cd /vagrant/rakbuku'
+	- Run 'sudo npm install'
+	- Run 'bower install'
+# Run site
+	- Run 'grunt serve'
+	- Open browser and run with '192.168.33.25:9000'
--- rakbuku/app/scripts/main.js
+++ rakbuku/app/scripts/main.js
@@ -5,215 +5,262 @@ var Rakbuku = {};
 
 (function($, W, R) {
 
-	R.app = function() {
+    R.app = function() {
 
-		var mainLogo = $('.logo'),
-			heroInit = $('.hero'),
-			services = $('.is-services'),
-			about = $('.is-about'),
-			outwork = $('.is-outwork'),
-			contact = $('.is-contact'),
+        var mainLogo = $('.logo'),
+            isHero = '.hero',
+            isServices = '.is-services',
+            isOutwork = '.is-outwork',
+            isAbout = '.is-about',
+            isContact = '.is-contact',
+            none = 'none-display',
 
-			/*All actions of home screen*/
-			homeAction = function() {
-				var logo = $('.logo__a--main'),
-					menu = $('.menu-items'),
-					heroInitLeft = $('.hero__indct--left'),
-					heroInitRight = $('.hero__indct--right');
+            /*All actions of home screen*/
+            homeScript = function() {
 
-				logo.removeClass('is-bounceInDown');
-				menu.removeClass('is-fadeInRight');
-				heroInitLeft.removeClass('is-rotateInDownRight');
-				heroInitRight.removeClass('is-rotateInDownLeft');
+                var logo = $('.logo__a--main'),
+                    menu = $('.menu-items'),
+                    heroInitLeft = $('.hero__indct--left'),
+                    heroInitRight = $('.hero__indct--right'),
+                    bounceInDown = 'is-bounce-in-down',
+                    fadeInRight = 'is-fade-in-right',
+                    rotateInDownRight = 'is-rotate-in-down-right',
+                    rotateInDownLeft = 'is-rotate-in-down-left';
 
-				logo.addClass('is-bounceInDown');
+                logo.removeClass(bounceInDown);
+                menu.removeClass(fadeInRight);
+                heroInitLeft.removeClass(rotateInDownRight);
+                heroInitRight.removeClass(rotateInDownLeft);
+                logo.addClass(bounceInDown);
 
-				if (!menu.hasClass('is-fadeInRight')) {
-					setTimeout(function() {
-						menu.addClass('is-fadeInRight');
-					}, 1000);
-				}
+                if (!menu.hasClass(fadeInRight)) {
+                    setTimeout(function() {
+                        menu.addClass(fadeInRight);
+                    }, 1000);
+                }
 
-				setTimeout(function() {
-					heroInitLeft.addClass('is-rotateInDownRight');
-				}, 2000);
+                setTimeout(function() {
+                    heroInitLeft.addClass(rotateInDownRight);
+                }, 2000);
 
-				setTimeout(function() {
-					heroInitRight.addClass('is-rotateInDownLeft');
-				}, 3000);
-			},
+                setTimeout(function() {
+                    heroInitRight.addClass(rotateInDownLeft);
+                }, 3000);
+            },
 
-			/*Actions of menu*/
-			menuActive = function() {
-				$('.menu-items .item__link').on('click', function(event) {
-					if (!$(this).hasClass('item--active')) {
-						window.scrollTo(0, 0);
-						var isActive = $(this).attr('href');
-						$('.menu-items .item__link').removeClass('item--active');
-						$(this).addClass('item--active');
-						switch (isActive) {
-							case '#home':
-								if ($('.logo').hasClass('none-display')) {
-									$('.logo').removeClass('none-display');
-								};
-								itemsActive('.hero');
-								break;
-							case '#services':
-								if (!$('.logo').hasClass('none-display')) {
-									$('.logo').addClass('none-display');
-								};
-								itemsActive('.is-services');
-								servicesActive();
-								break;
-							case '#outwork':
-								if (!$('.logo').hasClass('none-display')) {
-									$('.logo').addClass('none-display');
-								};
-								itemsActive('.is-outwork');
-								outworkActive();
-								break;
-							case '#about':
-								if (!$('.logo').hasClass('none-display')) {
-									$('.logo').addClass('none-display');
-								};
-								itemsActive('.is-about');
-								aboutActive();
-								break;
-							case '#contact':
-								if (!$('.logo').hasClass('none-display')) {
-									$('.logo').addClass('none-display');
-								};
-								itemsActive('.is-contact');
-								contactActive();
-								break;
-						}
+            /*Actions of menu*/
+            menuScript = function() {
 
-					};
+                $('.menu-items .item__link').on('click', function(event) {
 
-				});
-			},
+                    var itemActive = 'item--active';
 
-			/*Actions of the services screen*/
-			servicesActive = function() {
-				var webDesign = $('.js-webdesign'),
-					webDevelopment = $('.js-webdevelopment'),
-					webResponsive = $('.js-responsive'),
-					pcImage = $('.services--images-item--pc'),
-					tabletImage = $('.services--images-item--tablet'),
-					mobileImage = $('.services--images-item--mobile');
+                    if (!$(this).hasClass(itemActive)) {
 
-				webDesign.add(webDevelopment).add(webResponsive).removeClass('is-fadeInLeft');
-				pcImage.add(tabletImage).add(mobileImage).removeClass('is-fadeInDownBig');
+                        var isActive = $(this).attr('href');
 
-				setTimeout(function() {
-					webDesign.addClass('is-fadeInLeft');
-					pcImage.addClass('is-fadeInDownBig');
-				}, 1000);
-				setTimeout(function() {
-					webDevelopment.addClass('is-fadeInLeft');
-					tabletImage.addClass('is-fadeInDownBig');
-				}, 2000);
-				setTimeout(function() {
-					webResponsive.addClass('is-fadeInLeft');
-					mobileImage.addClass('is-fadeInDownBig');
-				}, 3000);
+                        window.scrollTo(0, 0);
+                        $('.menu-items .item__link').removeClass(itemActive);
+                        $(this).addClass(itemActive);
 
-			},
+                        switch (isActive) {
+                            case '#home':
+                                displayLogo();
+                                itemsActive(isHero);
+                                break;
+                            case '#services':
+                                hiddenLogo();
+                                itemsActive(isServices);
+                                servicesScript();
+                                break;
+                            case '#outwork':
+                                hiddenLogo();
+                                itemsActive(isOutwork);
+                                outworkScript();
+                                break;
+                            case '#about':
+                                hiddenLogo();
+                                itemsActive(isAbout);
+                                aboutScript();
+                                break;
+                            case '#contact':
+                                hiddenLogo();
+                                itemsActive(isContact);
+                                contactScript();
+                                break;
+                        }
 
-			/*Actions of the about us screen*/
-			aboutActive = function() {
-				var ceo = $('.js-ceo'),
-					designer = $('.js-designer'),
-					developer = $('.js-developer'),
-					aboutDisc = $('.about--disc'),
-					aboutList = $('.about-list-title');
+                    };
 
-				ceo.add(designer).add(developer).removeClass('is-fadeIn');
+                });
+            },
 
-				aboutDisc.removeClass('is-bounceInDown');
-				aboutList.removeClass('is-bounceIn');
+            /*Actions of the services screen*/
+            servicesScript = function() {
 
-				setTimeout(function() {
-					aboutDisc.addClass('is-bounceInDown');
-				}, 1000);
-				setTimeout(function() {
-					aboutList.addClass('is-bounceIn');
-				}, 2000);
-				setTimeout(function() {
-					ceo.addClass('is-fadeIn');
-				}, 3000);
-				setTimeout(function() {
-					designer.addClass('is-fadeIn');
-				}, 4000);
-				setTimeout(function() {
-					developer.addClass('is-fadeIn');
-				}, 5000);
-			},
+                var webDesign = $('.js-webdesign'),
+                    webDevelopment = $('.js-webdevelopment'),
+                    webResponsive = $('.js-responsive'),
+                    pcImage = $('.services--images-item--pc'),
+                    tabletImage = $('.services--images-item--tablet'),
+                    mobileImage = $('.services--images-item--mobile'),
+                    fadeInLeft = 'is-fade-in-left',
+                    fadeInDownBig = 'is-fade-in-down-big';
 
-			/*Actions of the outwork screen*/
-			outworkActive = function() {
-				var outworkItems = $('.outworks__item');
-				setTimeout(function() {
-					for (var i = 0, len = outworkItems.length; i < len; i++) {
-						$(outworkItems[i]).addClass('is-bounceIn');
-					}
-				}, 1000);
-			},
+                webDesign.add(webDevelopment).add(webResponsive).removeClass(fadeInLeft);
+                pcImage.add(tabletImage).add(mobileImage).removeClass(fadeInDownBig);
 
-			/*Actions of the contact creen*/
-			contactActive = function() {
-				var name = $('#name'),
-					email = $('#email'),
-					subject = $('#subject'),
-					messages = $('#messages'),
-					submit = $('#submit');
+                setTimeout(function() {
+                    webDesign.addClass(fadeInLeft);
+                    pcImage.addClass(fadeInDownBig);
+                }, 1000);
 
-				name.add(email).add(subject).add(messages).add(submit).removeClass('is-bounceIn');
+                setTimeout(function() {
+                    webDevelopment.addClass(fadeInLeft);
+                    tabletImage.addClass(fadeInDownBig);
+                }, 2000);
 
-				setTimeout(function() {
-					name.addClass('is-bounceIn');
-				}, 1000);
-				setTimeout(function() {
-					email.addClass('is-bounceIn');
-				}, 2000);
-				setTimeout(function() {
-					subject.addClass('is-bounceIn');
-				}, 3000);
-				setTimeout(function() {
-					messages.addClass('is-bounceIn');
-				}, 4000);
-				setTimeout(function() {
-					submit.addClass('is-bounceIn');
-				}, 5000);
-			},
-			itemsActive = function(item) {
-				var items = ['.hero', '.is-services', '.is-about', '.is-outwork', '.is-contact'];
-				for (var i = 0, len = items.length; i < len; i++) {
-					if (items[i] != item) {
-						if (!$(items[i]).hasClass('none-display')) {
-							$(items[i]).addClass('none-display');
-						};
-					} else {
-						if ($(items[i]).hasClass('none-display')) {
-							$(items[i]).removeClass('none-display');
-						}
-					}
-				}
-			};
+                setTimeout(function() {
+                    webResponsive.addClass(fadeInLeft);
+                    mobileImage.addClass(fadeInDownBig);
+                }, 3000);
 
-		//###############################
-		// Public methods
-		//###############################
-		this.init = function() {
-			homeAction();
-			menuActive();
-		};
-	};
+            },
+
+            /*Actions of the about us screen*/
+            aboutScript = function() {
+
+                var ceo = $('.js-ceo'),
+                    designer = $('.js-designer'),
+                    developer = $('.js-developer'),
+                    aboutDisc = $('.about--disc'),
+                    aboutList = $('.about-list-title'),
+                    fadeIn = 'is-fade-in',
+                    bounceInDown = 'is-bounce-in-down',
+                    bounceIn = 'is-bounce-in';
+
+                ceo.add(designer).add(developer).removeClass(fadeIn);
+                aboutDisc.removeClass(bounceInDown);
+                aboutList.removeClass(bounceIn);
+
+                setTimeout(function() {
+                    aboutDisc.addClass(bounceInDown);
+                }, 1000);
+
+                setTimeout(function() {
+                    aboutList.addClass(bounceIn);
+                }, 2000);
+
+                setTimeout(function() {
+                    ceo.addClass(fadeIn);
+                }, 3000);
+
+                setTimeout(function() {
+                    designer.addClass(fadeIn);
+                }, 4000);
+
+                setTimeout(function() {
+                    developer.addClass(fadeIn);
+                }, 5000);
+
+            },
+
+            /*Actions of the outwork screen*/
+            outworkScript = function() {
+
+                var outworkItems = $('.outworks__item'),
+                    bounceIn = 'is-bounce-in';
+
+                setTimeout(function() {
+                    for (var i = 0, len = outworkItems.length; i < len; i++) {
+                        $(outworkItems[i]).addClass(bounceIn);
+                    }
+                }, 1000);
+
+            },
+
+            /*Actions of the contact creen*/
+            contactScript = function() {
+
+                var name = $('#name'),
+                    email = $('#email'),
+                    subject = $('#subject'),
+                    messages = $('#messages'),
+                    submit = $('#submit'),
+                    bounceIn = 'is-bounce-in';
+
+                name.add(email).add(subject).add(messages).add(submit).removeClass(bounceIn);
+
+                setTimeout(function() {
+                    name.addClass(bounceIn);
+                }, 1000);
+
+                setTimeout(function() {
+                    email.addClass(bounceIn);
+                }, 2000);
+
+                setTimeout(function() {
+                    subject.addClass(bounceIn);
+                }, 3000);
+
+                setTimeout(function() {
+                    messages.addClass(bounceIn);
+                }, 4000);
+
+                setTimeout(function() {
+                    submit.addClass(bounceIn);
+                }, 5000);
+
+            },
+
+            itemsActive = function(item) {
+
+                var items = [isHero, isServices, isAbout, isOutwork, isContact];
+
+                for (var i = 0, len = items.length; i < len; i++) {
+
+                    if (items[i] != item) {
+                        if (!$(items[i]).hasClass(none)) {
+                            $(items[i]).addClass(none);
+                        };
+                    } else {
+                        if ($(items[i]).hasClass(none)) {
+                            $(items[i]).removeClass(none);
+                        }
+                    }
+
+                }
+            },
+
+            hiddenLogo = function() {
+
+                if (!mainLogo.hasClass(none)) {
+                    mainLogo.addClass(none);
+                };
+
+            },
+
+            displayLogo = function() {
+
+                if (mainLogo.hasClass(none)) {
+                    mainLogo.removeClass(none);
+                };
+
+            };
+
+        //###############################
+        // Public methods
+        //###############################
+        this.init = function() {
+            homeScript();
+            menuScript();
+        };
+    };
 
 }(window.jQuery, window, Rakbuku));
 
 
 // call init function once document is ready.
 window.jQuery(document).ready(function() {
-	new Rakbuku.app().init();
+    new Rakbuku.app().init();
 });
